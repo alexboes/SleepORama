5ee6ba1c9391d6338bc85566bf00b3120e42cdef
alexboes <alex_boes@verizon.net>
2015-10-22 10:53:29 -0400

    Here we go

--- app/src/main/java/edu/usf/cse/alexander/sleeporama/LoginActivity.java
+++ app/src/main/java/edu/usf/cse/alexander/sleeporama/LoginActivity.java
@@ -1,9 +1,12 @@
 package edu.usf.cse.alexander.sleeporama;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,6 +14,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button login = (Button) this.findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendUsername();
+            }
+        });
     }
 
     @Override
@@ -33,6 +44,12 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void sendUsername()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
 
